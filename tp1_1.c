5e8bb38651da7ad736ea88ced6a0f07c695c2abf
Qhari Waman <qhariquinteros@gmail.com>
2025-03-31 18:02:23 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Mundo!!");
+    char letra='Q';
+    char *puntero;
+    puntero = &letra;
+
+    printf("Hola Mundo!!\n");
+
+    printf("El contenido del puntero: %c\n", *puntero);
+    printf("La direccion de memoria almacenada por el puntero: %p\n", puntero);
+    printf("La direccion de memoria de la variable: %p\n", &letra);
+    printf("La direccion de memoria del puntero: %p\n", &puntero);
+    printf("Tamanio de la variable letra= %zu bytes", sizeof(letra));
+
+
     return 0;
 }
